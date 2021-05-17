059494c1cf8913de05892be73817cdcefb9e7520
EVANS-LAPTOP123\Juanjuan Zhang <susanzhwd@gmail.com>
2021-05-17 14:46:26 -0400

    discard lifecycle

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./App.css";
 import App from "./App";
-import Lifecycle from "./Lifecycle";
 import reportWebVitals from "./reportWebVitals";
 // import "bootstrap/dist/css/bootstrap.min.css";
 
-// ReactDOM.render(<App />, document.getElementById("root"));
-ReactDOM.render(<Lifecycle />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
